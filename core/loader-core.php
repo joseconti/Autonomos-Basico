7d8c10d6bf7b5098ee161c183e9c08bcce512dae
José Conti <j.conti@joseconti.com>
2018-02-13 14:11:36 +0100

    load-core.php update

--- core/loader-core.php
+++ core/loader-core.php
@@ -1,4 +1,5 @@
 <?php
 	if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-	require_once( AUTONOMOS_PLUGIN_PATH . 'core/functions/functions.php' );
+
+	require_once( AUTONOMOS_PLUGIN_PATH . 	'core/functions/functions.php'		 );
