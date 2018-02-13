aa8eab798627468f97d17f925653a62cc6b97427
José Conti <j.conti@joseconti.com>
2018-02-13 14:10:09 +0100

    Updated loader.php

--- loader.php
+++ loader.php
@@ -1,37 +1,37 @@
 <?php
-    /*
-    Plugin Name: Autonomos
-    Plugin URI: https://www.joseconti.com
-    Description: Plugins for spaniards "Autonomos"
-    Version: 1.3.0-rc2
-    Author: Jose Conti
-    Author URI: https://www.joseconti.com
-    Tested up to: 4.9.3
-    Text Domain: autonomos
-    Domain Path: /languages/
-    License: GPLv2 only
-    License URI: http://www.gnu.org/licenses/gpl-2.0.html
-    */
+/*
+Plugin Name: Autonomos
+Plugin URI: https://www.joseconti.com
+Description: Plugins for spaniards "Autonomos"
+Version: 1.3.0-rc4
+Author: Jose Conti
+Author URI: https://www.joseconti.com
+Tested up to: 4.9.3
+Text Domain: autonomos
+Domain Path: /languages/
+License: GPLv2 only
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
 
-    define( 'AUTONOMOS_VERSION',        '1.3.0-rc2'                     );
-    define( 'AUTONOMOS_PLUGIN_PATH',    plugin_dir_path( __FILE__ ) );
-    define( 'AUTONOMOS_PLUGIN_URL',     plugin_dir_url( __FILE__ )  );
+	define( 'AUTONOMOS_VERSION', '1.3.0-rc4' );
+	define( 'AUTONOMOS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+	define( 'AUTONOMOS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-    add_action('plugins_loaded', 'autonomos_init', 0);
+	add_action( 'plugins_loaded', 'autonomos_init', 11 );
 
-    if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 
-        include_once( AUTONOMOS_PLUGIN_PATH . 'core/loader-core.php' );
+	function autonomos_init() {
+	    load_plugin_textdomain( 'autonomos', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	    if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	        include_once( AUTONOMOS_PLUGIN_PATH . 'core/loader-core.php' );
+	    }
+	}
 
-        add_filter( 'woocommerce_get_settings_pages', 'autonomos_include_woocommerce_setting_file' );
-        function autonomos_include_woocommerce_setting_file( $settings ) {
+	add_filter( 'woocommerce_get_settings_pages', 'autonomos_include_woocommerce_setting_file' );
+	function autonomos_include_woocommerce_setting_file( $settings ) {
 
-            $settings[] = include( AUTONOMOS_PLUGIN_PATH .  'core/woocommerce/woo-functions.php' );
-            return $settings;
+	    $settings[] = include( AUTONOMOS_PLUGIN_PATH . 'core/woocommerce/woo-functions.php' );
 
-        }
-    }
+	    return $settings;
 
-    function autonomos_init() {
-        load_plugin_textdomain( 'autonomos', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-    }
+	}
